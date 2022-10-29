b5a224d2b221603fcf8b1368b87298ee2582636e
marina <nicolaimarina@gmail.com>
2022-10-29 10:59:26 -0400

    api info necessary

--- pages/coin/[id].tsx
+++ pages/coin/[id].tsx
@@ -16,8 +16,21 @@ export default function UserPage() {
 
   console.log(data);
   return (<>
-   <div>{data.name}</div>
-   <div>test</div>
+   <h3>{data.name}</h3>
+   <h3>image</h3>
+   <h3>price: USD, GBP, EUR , BTC </h3>
+   <h3>price increase % in 24H</h3>
+   <h3>price decrease % in 24H</h3>
+   <h3>All time high/: date </h3>
+   <h3>All time low: date</h3>
+   <h3>Market Cap:</h3>
+   <h3>Fully Diluted Valuation:</h3>
+   <h3>Volume 24h:</h3>
+   <h3>Volume / Market:</h3>
+   <h3>Total Volume:</h3>
+   <h3>Circulating Supply:</h3>
+   <h3>Max Supply:</h3>
+   <h3>website for more info</h3>
    </>
   )
 }
